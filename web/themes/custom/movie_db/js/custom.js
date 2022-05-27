256a0857d45cb8b1508beecacf9790ea249a79c3
Binh Hoang <binh.hoang@codeenginestudio.com>
2022-05-27 14:12:46 +0700

    task week 8

--- web/themes/custom/movie_db/js/custom.js
+++ web/themes/custom/movie_db/js/custom.js
@@ -6,11 +6,22 @@
 (function($, Drupal) {
 
   'use strict';
+  $(document).ready(function() {
+    $('[data-toggle=tooltip]').tooltip();
+  });
 
   Drupal.behaviors.movie_db = {
     attach: function(context, settings) {
 
-      // Custom code here
+      var ids = $('.tooltip-val').map(function (){
+        return this.id;
+      });
+      for (let index = 0; index < ids.length; index++) {
+        let vals = $('#'+ids[index]).text();
+        $('.tooltip-active-'+ids[index]).attr('title',vals);
+        //console.log(vals);
+
+      }
 
     }
   };
